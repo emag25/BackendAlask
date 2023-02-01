cf772b2ded8798a5ccea24e15ad45f524e0b2728
Pau <paulitayg@hotmail.com>
2023-02-01 14:49:05 -0500

    Clase y controlador usuario

---
+++ AlaskSolucion/Alask.API/Controllers/UsuariosController.cs
@@ -0,0 +1,32 @@
+﻿using Alask.API.CodeGeneral;
+using Alask.BL;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Xml.Linq;
+
+
+namespace Alask.API
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UsuariosController : Controller
+    {
+
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ActionResult<Usuarios>> GetUsuariosTodos([FromBody] Usuario usuarios)
+        {
+            var cadenaConexion = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["conexion_bd"];
+            XDocument xmlParam = DBXmlMethods.GetXml(proveedores);
+            DataSet dsResultado = await DBXmlMethods.EjecutaBase("Usuarios_GetUsuarios", cadenaConexion, "consultar_todo", xmlParam.ToString());
+            List<Usuario> listData = new List<Usuario>();
+
+            return Ok(listData);
+        }
+
+
+    }
+
