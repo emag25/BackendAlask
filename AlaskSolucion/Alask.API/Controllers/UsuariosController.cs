b7d0e22f7ba3a901ed7c0f0fa577c4c4993405ae
Pau <paulitayg@hotmail.com>
2023-02-10 15:24:37 -0500

    GET YA

--- AlaskSolucion/Alask.API/Controllers/UsuariosController.cs
+++ AlaskSolucion/Alask.API/Controllers/UsuariosController.cs
@@ -16,29 +16,85 @@ namespace Alask.API
     {
 
 
-        [Route("[action]")]
+        [Route("[action]/{nombreprocedimiento}")]
         [HttpPost]
-        public async Task<ActionResult<Usuarios>> GetUsuariosTodos(Usuarios usuarios)
+        public async Task<ActionResult<Usuarios>> GetUsuarios(Usuarios usuarios, string nombreprocedimiento)
         {
-            var cadenaConexion = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["conexion_bd"];
-            XDocument xmlParam = DBXmlMethods.GetXml(usuarios);
-            DataSet dsResultado = await DBXmlMethods.EjecutaBase("user_Getuser", cadenaConexion, "consultar_todo", xmlParam.ToString());
-
-            List<Usuarios> listData = new List<Usuarios>();
-
-            if (dsResultado.Tables.Count >= 0 || dsResultado.Tables[0].Rows.Count >=0)
+            Console.WriteLine(nombreprocedimiento);
+            if (nombreprocedimiento == "todo")
             {
-                string JSONstring = string.Empty;
-                JSONstring = JsonConvert.SerializeObject(dsResultado.Tables[0]);
-                return Ok(JSONstring);
+                var cadenaConexion = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["conexion_bd"];
+                XDocument xmlParam = DBXmlMethods.GetXml(usuarios);
+                DataSet dsResultado = await DBXmlMethods.EjecutaBase("user_Getuser", cadenaConexion, "consultar_todo", xmlParam.ToString());
 
-            }else
-            {
-                return Ok();
+                List<Usuarios> listData = new List<Usuarios>();
+
+                if (dsResultado.Tables.Count >= 0 || dsResultado.Tables[0].Rows.Count >= 0)
+                {
+                    string JSONstring = string.Empty;
+                    JSONstring = JsonConvert.SerializeObject(dsResultado.Tables[0]);
+                    return Ok(JSONstring);
+
+                }
+                else
+                {
+                    return Ok();
+                }
             }
+            else if (nombreprocedimiento == "apellido")
+            {
+                var cadenaConexion = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["conexion_bd"];
+                XDocument xmlParam = DBXmlMethods.GetXml(usuarios);
+                DataSet dsResultado = await DBXmlMethods.EjecutaBase("user_Getuser", cadenaConexion, "consultar_porApellido", xmlParam.ToString());
 
+                List<Usuarios> listData = new List<Usuarios>();
+
+                if (dsResultado.Tables[0].Rows.Count > 0)
+                {
+                    string JSONstring = string.Empty;
+                    JSONstring = JsonConvert.SerializeObject(dsResultado.Tables[0]);
+                    return Ok(JSONstring);
+
+                }
+                else
+                {
+                    return Ok();
+                }
+            }
+            else if (nombreprocedimiento == "email")
+            {
+                var cadenaConexion = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["conexion_bd"];
+                XDocument xmlParam = DBXmlMethods.GetXml(usuarios);
+                DataSet dsResultado = await DBXmlMethods.EjecutaBase("user_Getuser", cadenaConexion, "consultar_porEmail", xmlParam.ToString());
+
+                List<Usuarios> listData = new List<Usuarios>();
+
+                if (dsResultado.Tables[0].Rows.Count > 0)
+                {
+                    string JSONstring = string.Empty;
+                    JSONstring = JsonConvert.SerializeObject(dsResultado.Tables[0]);
+                    return Ok(JSONstring);
+
+                }
+                else
+                {
+                    return Ok();
+                }
+            }
+            else
+            {
+                return BadRequest();
+
+            }
 
         }
     }
+
+  
 }
+
+
+
+
+    
 
--- AlaskSolucion/Alask.BL/models/Usuarios.cs
+++ AlaskSolucion/Alask.BL/models/Usuarios.cs
@@ -2,15 +2,15 @@
 {
     public class Usuarios
     {
-        public int Id { get; set; }
-        public string? Nombre { get; set; }
-        public string? Apellido { get; set; }
-        public string? Email { get; set; }
-        public string? Password{ get; set; }
-        public string? Telefono { get; set; }
-        public string? Direccion { get; set; }
-        public Provincia? Provincia { get; set; }
-        public string? Rol { get; set; }
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public string? apellido { get; set; }
+        public string? email { get; set; }
+        public string? password{ get; set; }
+        public string? telefono { get; set; }
+        public string? direccion { get; set; }
+        public Provincia? provincia { get; set; }
+        public string? rol { get; set; }
         public DateOnly FechaIngreso { get; set; }
 
     }
