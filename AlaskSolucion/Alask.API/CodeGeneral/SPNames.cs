5766e735e94e87552822de128e66d7a4b3fbe85d
EMA <emelyapraez25@gmail.com>
2023-03-14 11:37:47 -0500

    SPnames x22222

---
+++ AlaskSolucion/Alask.API/CodeGeneral/SPNames.cs
@@ -0,0 +1,31 @@
+﻿namespace Alask.API.CodeGeneral
+{
+    public class SPNames
+    {
+        public const string GetProductos = "Productos_GetProductos";
+
+        public const string GetCategorias = "Categorias_GetCategorias";
+
+        public const string GetFavoritos = "Favoritos_GetFavoritos";
+
+        public const string GetProvincias = "GetProvincias";
+
+        public const string GetProveedores = "GetProveedores";
+
+        public const string GetSolicitudes = "GetSolicitudes";
+
+        public const string GetCarritos = "Carritos_getCarritos";
+
+        public const string GetVentas = "Ventas_GetVentas";
+
+        public const string SetCategorias = "Categoria_setCategoria";
+
+        public const string SetProductos = "Producto_setProducto";
+
+        public const string SetFavoritos = "Favorito_setFavorito";
+
+        public const string SetProveedores = "SetProveedor";
+
+        public const string SetSolicitudes = "SetSolicitud";
+    }
+}
