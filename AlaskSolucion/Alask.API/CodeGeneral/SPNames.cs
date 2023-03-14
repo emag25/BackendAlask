d753676fd44a00d03b04d46bd07a6b5707265bf2
EMA <emelyapraez25@gmail.com>
2023-03-14 11:27:58 -0500

    SPnames

--- AlaskSolucion/Alask.API/CodeGeneral/SPNames.cs
+++ AlaskSolucion/Alask.API/CodeGeneral/SPNames.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:AlaskSolucion/Alask.API/CodeGeneral/Get Producto-Categoria-FAvorito.cs
+
 namespace Alask.API.CodeGeneral
 {
     public class SPNames
@@ -14,34 +14,10 @@ namespace Alask.API.CodeGeneral
         public const string GetProveedores = "GetProveedores";
 
         public const string GetSolicitudes = "GetSolicitudes";
-
+        
         public const string GetCarritos = "Carritos_getCarritos";
 
         public const string GetVentas = "Ventas_GetVentas";
-        //
-        public const string SetCategorias = "Categoria_setCategoria";
-
-        public const string SetProductos = "Producto_setProducto";
-
-        public const string SetFavoritos = "Favorito_setFavorito";
-
-    }
-=======
-﻿namespace Alask.API.CodeGeneral
-{
-    public class SPNames
-    {
-        public const string GetProductos = "Productos_GetProductos";
-
-        public const string GetCategorias = "Categorias_GetCategorias";
-
-        public const string GetFavoritos = "Favoritos_GetFavoritos";
-
-        public const string GetProvincias = "GetProvincias";
-
-        public const string GetProveedores = "GetProveedores";
-
-        public const string GetSolicitudes = "GetSolicitudes";
         
         public const string SetCategorias = "Categoria_setCategoria";
 
@@ -53,5 +29,4 @@ namespace Alask.API.CodeGeneral
 
         public const string SetSolicitudes = "SetSolicitud";
     }
->>>>>>> Ruben:AlaskSolucion/Alask.API/CodeGeneral/SPNames.cs
 }
